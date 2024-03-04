3326f21cca5741cba0664d867d45859a61cd0a1d
udani99 <136361196+udani99@users.noreply.github.com>
2024-03-04 16:47:24 +0530

    Add files via upload

---
+++ Personal Portfolio/script.jsx
@@ -0,0 +1,7 @@
+var typed= new Typed(".textc", {
+    strings: ["A UI/UX Designer" , "A Graphic Designer" , "A Web Designer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop:true,
+});
